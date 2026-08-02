b8a958a1b02b1c898fe78f27288bfe8883366e6c
kanhaiyaML <personalkanhaiya2008@gmail.com>
2026-08-02 12:11:13 +0000

    add config.py

--- makima/config.py
+++ makima/config.py
@@ -1,12 +1,12 @@
 import os
 import sys
-from makima.setup import setup_api_key
+from makima.setup import setup_api_key,setup_model
 from dotenv import load_dotenv
 load_dotenv()
 
 GROQ_API_KEY = setup_api_key()
 # GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-MODEL_NAME = "qwen/qwen3.6-27b"
+MODEL_NAME = setup_model()
 TOKEN_LIMIT = 10000
 # PROJECT_ROOT = os.getcwd()
 PROJECT_ROOT = sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
